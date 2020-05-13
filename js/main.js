5746618a2d404678e256503da983af450b4172de
AirtonSilveira <a.silveira@accesscredito.com.br>
2020-05-13 17:10:56 -0400

    Aray e Dicionario

--- js/main.js
+++ js/main.js
@@ -3,5 +3,30 @@ var idade = 31;
 alert(nome + "tem " + idade + "anos");
 
 
+//Aray sao Listas ordenadas ou dsordenadas
+
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva") //Nesse exemplo estamos incluindo mais um elemento na lista
+lista.pop(); //Retira o ultimo elemento da lista
+console.log(lista); //Nesse caso imprimi toda a lista
+//console.log(lista.length); - me diz o tamanho da minha lista
+//console.log(lista.reverse()); - Traz os elementos ao contrario na minha lista
+//console.log(lista.join("|")) - neste exemplo ele faz o tipo de espaço que queremos entre os elementos da lista
+//console.log(lista[0]) - neste exemplo imprime o elemento escolhido da lista, neste caso a maça 
+
+
+//Dicionario
+
+/*var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+//Lista de Dicionario
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+ 
+
 
 
