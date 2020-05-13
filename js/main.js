67b65d4ce40d3e1f9cc03f225a86a2e2a7e73f2e
AirtonSilveira <a.silveira@accesscredito.com.br>
2020-05-13 17:41:17 -0400

    atualizando a uula

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var nome = "Airton Silveira";
+/*var nome = "Airton Silveira";
 var idade = 31;
-alert(nome + "tem " + idade + "anos");
+alert(nome + "tem " + idade + "anos");*/
 
-
+//ARREYS E DICIONARIOS
 //Aray sao Listas ordenadas ou dsordenadas
 
 
-var lista = ["maça", "pera", "laranja"];
-lista.push("uva") //Nesse exemplo estamos incluindo mais um elemento na lista
-lista.pop(); //Retira o ultimo elemento da lista
-console.log(lista); //Nesse caso imprimi toda a lista
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva") //Nesse exemplo estamos incluindo mais um elemento na lista
+//lista.pop(); //Retira o ultimo elemento da lista
+//console.log(lista); //Nesse caso imprimi toda a lista
 //console.log(lista.length); - me diz o tamanho da minha lista
 //console.log(lista.reverse()); - Traz os elementos ao contrario na minha lista
 //console.log(lista.join("|")) - neste exemplo ele faz o tipo de espaço que queremos entre os elementos da lista
@@ -23,10 +23,42 @@ console.log(fruta.nome);
 alert(fruta.cor);*/
 
 //Lista de Dicionario
-var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
-console.log(frutas);
-alert(frutas[1].nome);
- 
+//var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+//Condicionaus de repeticao e date
+
+/*var idade = prompt("Qual sua idade");
+if (idade >=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+//Estrtura de repeticao
+
+/*var count = 0;  //Esse script é um contador
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+//Outro exemplo de repeticao
+
+/*var count;
+for(count>0; count<= 5; count++){
+    alert(count);
+}*/
+
+//Imprimindo a data
+
+/*var d = new Date();
+alert(d());
+alert(d.getHours());*/ //Nesse caso imprime apena a hora
+
 
 
 
